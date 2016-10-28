a66dec914e39715f1847888a4ab7bf9300f2e1e0
xdialogx <hrinalds@gmail.com>
2016-10-28 12:56:36 +0300

    URL manager added

--- config/console.php
+++ config/console.php
@@ -22,6 +22,15 @@ $config = [
         ],
         'db' => $db,
     ],
+    'urlManager' => [
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+        'rules' => [
+            '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+            '<_c:[\w\-]+>' => '<_c>/index',
+            '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
+        ],
+    ],
     'params' => $params,
     /*
     'controllerMap' => [
--- config/web.php
+++ config/web.php
@@ -37,15 +37,16 @@ $config = [
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
             ],
         ],
-        */
+        'db' => require(__DIR__ . '/db.php'),
     ],
     'params' => $params,
 ];
